b7c4fb189dde791db46bca5f5daf28e865fdcf0d
James Kassemi <james@kassemi.org>
2025-11-09 19:26:44 -0500

    refactor: update classifier imports and formatting

--- classifier/src/lib.rs
+++ classifier/src/lib.rs
@@ -1,6 +1,6 @@
 // Copyright (c) James Kassemi, SC, US. All rights reserved.
 
-use core_types::{ClassParams, TradeLike};
+use core_types::types::{ClassParams, TradeLike};
 use nbbo_cache::NbboStore;
 
 /// Stub for stateless aggressor classifier.
@@ -13,7 +13,12 @@ impl Classifier {
     }
 
     /// Classify aggressor for a trade using NBBO store and params.
-    pub fn classify_trade(&self, trade: &mut dyn TradeLike, nbbo: &NbboStore, params: &ClassParams) {
+    pub fn classify_trade(
+        &self,
+        trade: &mut dyn TradeLike,
+        nbbo: &NbboStore,
+        params: &ClassParams,
+    ) {
         // Stub: No-op for now
     }
 }
